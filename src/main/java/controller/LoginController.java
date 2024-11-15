61b84a38dc0c1eb7ee8e229c8eeebf24e71c9088
DAM2 <DAM2@E36-21>
2024-11-15 12:03:48 +0100

    metodo checkeo usuario actualizado

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,6 +2,13 @@ package controller;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.ClientesDAO;
 import view.Inicio;
 import view.Login;
@@ -80,17 +87,46 @@ public class LoginController {
 //a estos metodos los llamo desde el listener del constructor
 
     private void autenticarUsuario() {
-        if (checkUser(vista.userForm.getText(), new String(vista.passForm.getPassword()))) {
-            vista.dispose();
-            Inicio inicio = new Inicio();
-            new InicioController(inicio);
-            inicio.setVisible(true);
-        } else {
-            vista.mensajeError.setVisible(true);
+        try {
+            if (checkUser(vista.userForm.getText(), new String(vista.passForm.getPassword()))) {
+                vista.dispose();
+                Inicio inicio = new Inicio();
+                new InicioController(inicio);
+                inicio.setVisible(true);
+            } else {
+                vista.mensajeError.setVisible(true);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
         }
     }
 
-    public boolean checkUser(String user, String pass) {
-        return "dani".equals(user) && "1234".equals(pass);
+    public boolean checkUser(String correo, String contra) throws IOException {
+        BufferedReader fich = null;
+        String linea;
+        boolean existeCombo = false;
+        try {
+            fich = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\DAM2\\Downloads\\FINALINFERFACE\\src\\main\\resources\\clientes.txt")));
+            linea = fich.readLine();
+            while (linea != null && !existeCombo) {
+                existeCombo = linea.equals(correo + ":" + contra);
+                linea = fich.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        } finally {
+            if (fich != null) {
+                try {
+                    fich.close();
+                } catch (IOException ex) {
+
+                    System.out.println(ex);
+                }
+            }
+        }
+
+        return existeCombo;
     }
 }
